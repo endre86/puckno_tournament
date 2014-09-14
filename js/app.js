c9c7cdf8ad1af4046f73e493e7769244ca758e51
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-14 17:35:31 +0200

    removed commented text

--- js/app.js
+++ js/app.js
@@ -38,24 +38,4 @@
 
 			$urlRouterProvider.otherwise('/');
 		}]);
-
-	// app.config(['$routeProvider',  
-	// 	function($routeProvider) {
-	// 		$routeProvider
-	// 		.when('/', {
-	// 			templateUrl: 'templates/all-tournaments.html',
-	// 			controller: 'AllTournamentsCtrl'
-	// 		})
-	// 		.when('/tournament/:tournamentId', {
-	// 			templateUrl: 'templates/tournament-info.html',
-	// 			controller: 'TournamentInfoCtrl'
-	// 		})
-	// 		.when('/tournament/:tournamentId/players', {
-	// 			templateUrl: 'templates/tournament-players.html',
-	// 			controller: 'RegisteredPlayersCtrl'
-	// 		})
-	// 		.otherwise({
-	// 			redirectTo: '/'
-	// 		});
-	// 	}]);
 })(angular);
