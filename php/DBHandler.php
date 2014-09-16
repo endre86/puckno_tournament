0a6735e7e40b9ca52210f68d8a8a288c8e12552f
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-16 03:15:36 +0200

    getTournamentInfo() added

--- php/DBHandler.php
+++ php/DBHandler.php
@@ -1,12 +1,51 @@
 <?php
-	/**
-	 * 
-	 */
-	require('credentials.php'):
+	require('credentials.php');
 
 	class DBHandler {
 		private $mysqli;
 
-		public function __construct()
+		public function __construct($host, $user, $password, $database) {
+			$this->mysqli = new mysqli($host, $user, $password, $database);
+		}
+
+		public function __destruct() {
+			$this->mysqli->close();
+		}
+
+		public function getTournamentInformation($tournamentId) {
+			$stmt = $this->mysqli->prepare('SELECT * FROM tournaments WHERE id=? LIMIT 1');
+
+			$stmt->bind_param("s", $tournamentId);
+			$stmt->execute();
+			
+			$result = $stmt->get_result();
+			$resArray = array();
+			
+			$resArr = $result->fetch_array(MYSQLI_ASSOC);
+			return $this->arrayToJsonHelper($resArr);
+		}
+
+		/**
+		 * Used when a string in the array is
+		 * a json array or object.
+		 *
+		 * Ex: array['x'] = "[{\"key\" : \"value\"}]"
+		 */
+		private function arrayToJsonHelper($array) {
+			return '{' .
+				implode(',<br />', array_map(function($value, $key) {
+					if(substr($value, 0, 1) == '[' ||
+					   substr($value, 0, 1 == '{')) {
+						return '"' . $key . '" = ' . $value;
+					}
+
+					return '"' . $key . '" = "' . $value . '"';
+				}, $array, array_keys($array))) . '}';
+		}
+
 	}
+
+	$db = new DBHandler();
+	$res = $db->getTournamentInformation("jo14");
+	echo $res;
 ?>
