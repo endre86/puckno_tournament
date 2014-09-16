062cb6e8da3534ebcf6e3c58b8645edbd4300235
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-16 23:45:13 +0200

    getTournament(id) finished

--- php/TournamentController.php
+++ php/TournamentController.php
@@ -1,17 +1,20 @@
 <?php
-session_start();
-
-require('credentials.php');
-require('DBHandler.php')
+require_once('credentials.php');
+require_once('DBHandler.php');
 
 class TournamentController {
 
-	private $dbHandler:
+	private $dbHandler;
 
 	public function __construct() {
+		$this->dbHandler = new DBHandler(
+			DBCredentials::HOST, 
+			DBCredentials::USER, 
+			DBCredentials::PASSWORD, 
+			DBCredentials::DATABASE);
+	}
 
+	public function get($tournamentId) {
+		return $this->dbHandler->getTournament($tournamentId);
 	}
 }
-
-method:post
-url: api.php?controller
