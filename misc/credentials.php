259046386640ec0fa28a8f3152c3047e044d9134
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-16 03:38:53 +0200

    + credentials.php example

---
+++ misc/credentials.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Belongs in /php.
+ */
+class DBCredentials {
+	public $db_host = '';
+	public $db_database = '';
+	public $db_user = '';
+	public $db_user_password = '';
+	public $db_admin = '';
+	public $db_admin_password = '';
+}
+?>
