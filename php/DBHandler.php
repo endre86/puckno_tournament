6b4a322bab0c8d17237fde8361795f9cc42865f1
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-16 23:47:45 +0200

    Revert "getTournament(id) finished"
    This reverts commit 062cb6e8da3534ebcf6e3c58b8645edbd4300235.

--- php/DBHandler.php
+++ php/DBHandler.php
@@ -1,46 +1,50 @@
 <?php
+	class DBHandler {
+		private $mysqli;
 
-class DBHandler {
-	private $mysqli;
+		public function __construct($host, $user, $password, $database) {
+			$this->mysqli = new mysqli($host, $user, $password, $database);
+		}
 
-	public function __construct($host, $user, $password, $database) {
-		$this->mysqli = new mysqli($host, $user, $password, $database);
+		public function __destruct() {
+			$this->mysqli->close();
+		}
+
+		public function getTournamentInformation($tournamentId) {
+			$stmt = $this->mysqli->prepare('SELECT * FROM tournaments WHERE id=? LIMIT 1');
+
+			$stmt->bind_param("s", $tournamentId);
+			$stmt->execute();
+			
+			$result = $stmt->get_result();
+			$resArray = array();
+			
+			$resArr = $result->fetch_array(MYSQLI_ASSOC);
+			return $this->arrayToJsonHelper($resArr);
+		}
+
+		/**
+		 * Used when a string in the array is
+		 * a json array or object.
+		 *
+		 * Ex: array['x'] = "[{\"key\" : \"value\"}]"
+		 */
+		private function arrayToJsonHelper($array) {
+			return '{' .
+				implode(',', array_map(function($value, $key) {
+					if(substr($value, 0, 1) == '[' ||
+					   substr($value, 0, 1 == '{')) {
+						return '"' . $key . '":' . $value;
+					}
+
+					return '"' . $key . '":"' . $value . '"';
+				}, $array, array_keys($array))) . '}';
+		}
+
 	}
 
-	public function __destruct() {
-		$this->mysqli->close();
-	}
+	require('credentials.php');
+	$cr = new DBCredentials();
+	$db = new DBHandler($cr->db_host, $cr->db_user, $cr->db_user_password, $cr->db_database);
 
-	public function getTournament($tournamentId) {
-		$stmt = $this->mysqli->prepare('SELECT * FROM tournaments WHERE id=? LIMIT 1');
-
-		$stmt->bind_param("s", $tournamentId);
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
-		$resArray = array();
-		
-		$resArr = $result->fetch_array(MYSQLI_ASSOC);
-		return $this->arrayToJsonHelper($resArr);
-	}
-
-	/**
-	 * Used when a string in the array is
-	 * a json array or object.
-	 *
-	 * Ex: array['x'] = "[{\"key\" : \"value\"}]"
-	 */
-	private function arrayToJsonHelper($array) {
-		if(!is_array($array)) return '{}';
-		return '{' .
-			implode(',', array_map(function($value, $key) {
-				if(substr($value, 0, 1) == '[' ||
-				   substr($value, 0, 1 == '{')) {
-					return '"' . $key . '":' . $value;
-				}
-
-				return '"' . $key . '":"' . $value . '"';
-			}, $array, array_keys($array))) . '}';
-	}
-
-}
+	echo $db->getTournamentInformation('jo14');
--- php/TournamentController.php
+++ php/TournamentController.php
@@ -1,20 +1,17 @@
 <?php
-require_once('credentials.php');
-require_once('DBHandler.php');
+session_start();
+
+require('credentials.php');
+require('DBHandler.php')
 
 class TournamentController {
 
-	private $dbHandler;
+	private $dbHandler:
 
 	public function __construct() {
-		$this->dbHandler = new DBHandler(
-			DBCredentials::HOST, 
-			DBCredentials::USER, 
-			DBCredentials::PASSWORD, 
-			DBCredentials::DATABASE);
-	}
 
-	public function get($tournamentId) {
-		return $this->dbHandler->getTournament($tournamentId);
 	}
 }
+
+method:post
+url: api.php?controller
