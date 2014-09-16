92c3a9aaae7d82efa99b5e7ce780d36253785841
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-16 18:28:12 +0200

    implemented days left to tournament

--- js/filters.js
+++ js/filters.js
@@ -7,7 +7,7 @@
 		return function(datestamp) {
 			if(datestamp) {
 				var dateArr = datestamp.split('-');
-				var date = new Date(dateArr[0], dateArr[1], dateArr[2]);
+				var date = new Date(dateArr[0], dateArr[1] - 1, dateArr[2]);
 				return App.Utils.getNoDayName(date.getDay()) + ' ' +
 					   date.getDate() + '. ' + 
 					   App.Utils.getNoMonthName(date.getMonth()) + ' ' + 
@@ -16,5 +16,24 @@
 
 			return datestamp;
 		};
+	});
+
+	tournamentFilters.filter('daysUntil', function() {
+		return function (datestamp) {
+			if(datestamp) {
+				var dateArr = datestamp.split('-');
+
+				var now = new Date();
+				var then = new Date(dateArr[0], dateArr[1] - 1, dateArr[2]);
+				console.log(now);
+				console.log(then);
+
+				var oneDay = 24 * 60 * 60 * 1000;
+
+				var days = Math.round((then.getTime() - now.getTime()) / oneDay);
+
+				return days + 1; // correct for < 24 hours
+			}
+		}
 	});
 })(angular);
