55189923f257feb759a3cd8ca9be5379f51bea36
Endre Vestbø <endre.vestbo@gmail.com>
2014-09-16 23:49:57 +0200

    getTournament finished

--- php/DBHandler.php
+++ php/DBHandler.php
@@ -1,50 +1,46 @@
 <?php
-	class DBHandler {
-		private $mysqli;
 
-		public function __construct($host, $user, $password, $database) {
-			$this->mysqli = new mysqli($host, $user, $password, $database);
-		}
+class DBHandler {
+	private $mysqli;
 
-		public function __destruct() {
-			$this->mysqli->close();
-		}
-
-		public function getTournamentInformation($tournamentId) {
-			$stmt = $this->mysqli->prepare('SELECT * FROM tournaments WHERE id=? LIMIT 1');
-
-			$stmt->bind_param("s", $tournamentId);
-			$stmt->execute();
-			
-			$result = $stmt->get_result();
-			$resArray = array();
-			
-			$resArr = $result->fetch_array(MYSQLI_ASSOC);
-			return $this->arrayToJsonHelper($resArr);
-		}
-
-		/**
-		 * Used when a string in the array is
-		 * a json array or object.
-		 *
-		 * Ex: array['x'] = "[{\"key\" : \"value\"}]"
-		 */
-		private function arrayToJsonHelper($array) {
-			return '{' .
-				implode(',', array_map(function($value, $key) {
-					if(substr($value, 0, 1) == '[' ||
-					   substr($value, 0, 1 == '{')) {
-						return '"' . $key . '":' . $value;
-					}
-
-					return '"' . $key . '":"' . $value . '"';
-				}, $array, array_keys($array))) . '}';
-		}
-
+	public function __construct($host, $user, $password, $database) {
+		$this->mysqli = new mysqli($host, $user, $password, $database);
 	}
 
-	require('credentials.php');
-	$cr = new DBCredentials();
-	$db = new DBHandler($cr->db_host, $cr->db_user, $cr->db_user_password, $cr->db_database);
+	public function __destruct() {
+		$this->mysqli->close();
+	}
 
-	echo $db->getTournamentInformation('jo14');
+	public function getTournament($tournamentId) {
+		$stmt = $this->mysqli->prepare('SELECT * FROM tournaments WHERE id=? LIMIT 1');
+
+		$stmt->bind_param("s", $tournamentId);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		$resArray = array();
+		
+		$resArr = $result->fetch_array(MYSQLI_ASSOC);
+		return $this->arrayToJsonHelper($resArr);
+	}
+
+	/**
+	 * Used when a string in the array is
+	 * a json array or object.
+	 *
+	 * Ex: array['x'] = "[{\"key\" : \"value\"}]"
+	 */
+	private function arrayToJsonHelper($array) {
+		if(!is_array($array)) return '{}';
+		return '{' .
+			implode(',', array_map(function($value, $key) {
+				if(substr($value, 0, 1) == '[' ||
+				   substr($value, 0, 1 == '{')) {
+					return '"' . $key . '":' . $value;
+				}
+
+				return '"' . $key . '":"' . $value . '"';
+			}, $array, array_keys($array))) . '}';
+	}
+
+}
