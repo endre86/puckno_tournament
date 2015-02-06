97c509c0a34f423e5c4475fbacdf8978f724a67b
endre86 <endre.vestbo@gmail.com>
2015-02-07 00:13:23 +0100

    added logging and updated AuthenitcationController

--- php/AuthenticationController.php
+++ php/AuthenticationController.php
@@ -15,18 +15,18 @@ class AuthenticationController extends AbstractController {
 		if(password_verify($password, $pw)) {
 			$_SESSION[parent::ADMIN] = true;
 			$_SESSION[parent::SESSION_USERNAME] = $username;
-			$this->logger->info('Successfully logged in user ' . $username);
+			$this->logger->info('AuthenticationController: Successfully logged in user ' . $username);
 			return parent::createSuccessJSONObject();
 		}
 
-		$this->logger->info('Failed login attempt for ' . $username);
+		$this->logger->info('AuthenticationController: Failed login attempt for ' . $username);
 		return parent::createErrorJSONObject('Bad username / password combination');
 	}
 
 	public function logout() {
 		parent::verifyIsAdminOrExit();
 
-		$this->logger->debug('Logout called for ' . $_SESSION[parent::SESSION_USERNAME]);
+		$this->logger->debug('AuthenticationController: Logout called for ' . $_SESSION[parent::SESSION_USERNAME]);
 		unset($_SESSION[parent::ADMIN]);
 		unset($_SESSION[parent::SESSION_USERNAME]);
 		return parent::createSuccessJSONObject();
