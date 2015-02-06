a2e4a01cfef9a91fe0714dda7c28a5796e7718fb
endre86 <endre.vestbo@gmail.com>
2015-02-07 00:44:10 +0100

    added logging and updated TournamentController

--- php/TournamentController.php
+++ php/TournamentController.php
@@ -3,22 +3,29 @@ require('AbstractController.php');
 
 class TournamentController extends AbstractController {
 
-	public function __construct($api) {
-		parent::__construct($api);
+	public function __construct($api, $logger) {
+		parent::__construct($api, $logger);
 	}
 
 	public function get($id) {
 		parent::verifyIsUserOrExit();
+		
+		$this->logger->debug('TournamentController: Getting tournament with tournamentId: ' - $id);
+
 		$res = parent::getDBHandler()->getTournament($id);
 		$res['program'] = json_decode($res['program']);
 		$res['details'] = json_decode($res['details']);
 		$res['misc'] = json_decode($res['misc']);
+
+		$this->logger->debug('TournamentController: Got tournament ' - $res['id']);
 
 		return parent::toJson($res);
 	}
 
 	public function getList() {
 		parent::verifyIsUserOrExit();
+
+		$this->logger->debug('TournamentController: Getting tournament list.');
 		$res = parent::getDBHandler()->getTournamentsList();
 		return parent::toJson($res);
 	}
@@ -27,6 +34,9 @@ class TournamentController extends AbstractController {
 		$image, $misc, $program, $details) {
 		
 		parent::verifyIsAdminOrExit();
+
+		$this->logger->info('TournamentController: Create tournament called by ' . $_SESSION[parent::SESSION_USERNAME]);
+		$this->logger->info("($id, $name, $date)");
 
 		$res = parent::getDBHandler()->createTournament(
 			$id, $name, $language, $date, $deadline, $fee,
@@ -40,27 +50,26 @@ class TournamentController extends AbstractController {
 		
 		parent::verifyIsAdminOrExit();
 		
-		$name = $dataAssocArray['name'];
-		$language = $dataAssocArray['language'];
-		$date = $dataAssocArray['date'];
-		$deadline = $dataAssocArray['deadline'];
-		$fee = $dataAssocArray['fee'];
-		$venue = $dataAssocArray['venue'];
-		$contact = $dataAssocArray['contact'];
-		$image = $dataAssocArray['image'];
-		$misc = $dataAssocArray['misc'];
-		$program = $dataAssocArray['program'];
-		$details = $dataAssocArray['details'];
+		$this->logger->info('TournamentController: Update tournament called by ' . $_SESSION[parent::SESSION_USERNAME]);
+		$this->logger->info("($id, $name, $date)");
 
 		$res = parent::getDBHandler()->updateTournament(
 			$id, $name, $language, $date, $deadline, $fee,
 			$venue, $contact, $image, $misc, $program, $details);
 
+		$this->logger->debug('TournamentController: Update result: ' . $res);
 		return $parent::createResponseJSONObject($res);
 	}
 
 	public function delete($id) {
+		parent::verifyIsAdminOrExit();
+
+		$this->logger->info('TournamentController: Delete tournament called by ' . $_SESSION[parent::SESSION_USERNAME]);
+		$this->logger->info("($id, $name, $date)");
+
 		$res = parent::getDBHandler()->deleteTournament($id);
+
+		$this->logger->debug('TournamentController: Delete result: ' . $res);
 		return parent::createResponseJSONObject($res);
 	}
 }
